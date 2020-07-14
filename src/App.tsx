7645bbadd5fd8853d50cd1c9241f90ab44d03446
Mykhailo Tkachenko <mykhailo.tkachenko.ddi@gmail.com>
2020-07-14 15:00:05 +0300

    added weather widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
+import WeatherWidgetContainer from "./containers/WeatherWidgetContainer";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="container">
+      <h1>Wellcome to Weather App</h1>
+      <WeatherWidgetContainer />
+    </div>
+  );
 }
 
 export default App;
